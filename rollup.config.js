dffb0939c085ab490733ba3640daa55bb63bb8f6
Curt Tudor <curt@rentallect.com>
2022-03-27 21:44:59 -0400

    bump to ziti-browzer-sw-workbox-strategies 0.1.10

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,7 @@ export default {
     }),
     commonjs(),
     replace({
+      'preventAssignment': true,
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
     }),
     typescript(),
